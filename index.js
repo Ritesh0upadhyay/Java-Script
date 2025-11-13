86c51c585e8fee9f7bc5032664b825412ba63b22
Ritesh0upadhyay <enjeenierritesh1234@gmail.com>
2025-11-13 14:25:01 +0530

    adding index.html file

---
+++ index.js
@@ -0,0 +1,11 @@
+console.clear();
+console.log("Is this correct the clear function is working");
+console.log("Hello workd tis is isn't me mate it's just what we want\" to check the code")
+console.log("Now checking what comes when we want to have \\ in the code")
+const numberOfTimesRepeat = 10;
+const character ="😁";
+for(i=0;i<numberOfTimesRepeat;i++)
+    {
+     console.log("Added one char"+character);
+     console.log(character.repeat(i+1));
+    }
