cb5ce6683a4e8c9d6ebc6a53df3e2ec93ac939f4
Ritesh0upadhyay <enjeenierritesh1234@gmail.com>
2025-11-18 16:41:26 +0530

    Updated JS code

--- Calculator Project/calculator.js
+++ Calculator Project/calculator.js
@@ -1,26 +1,8 @@
 const screen= document.querySelector(".screen");
 let screenValue="";
-
-function add(a, b) {
-
-    return a + b;
-};
-function subtract(a, b) {
-    return a - b;
-};
-function multiply(a, b) {
-    return a * b;
-};
-function divide(a, b) {
-    if (b === 0) {
-        return "Error: Division by zero";
-    }else {
-        return a / b;
-    }};
-function c(a){
-    a=0;
-    return a;
-};
+runningTotal=0;
+let intScreenValue=0
+let lastOperator="";
 function buttonClick(value){
     if(isNaN(parseInt(value))){
         handleOperator(value);
@@ -33,19 +15,29 @@ function buttonClick(value){
 function handleOperator(operator){
     switch(operator){
         case "C":
+            
             screenValue="0";
+            runningTotal=0;
             break;      
-        case "=":
-            console.log("Equals");
+        case "=": 
+            intScreenValue = parseInt(screenValue);   // Get the number user entered
+            flushOperation(intScreenValue);           // Use correct value
+            screenValue = runningTotal.toString();    // Show result
+            runningTotal = 0;                         // Reset for next operation
+            lastOperator = "";
             break;
         case "←":
-            if(screenValue.length=== 1){screenValue=0}else{screenValue=screenValue.substring(0,screenValue.length-1)}; 
-           
+            if(screenValue.length=== 1)
+                {screenValue="0"}
+            else{
+                screenValue=screenValue.substring(0,screenValue.length-1)
+            }; 
             break;
         case "+":
         case "-":
         case "×":
         case "÷":
+            
             handleMath(operator);
             break;
         default:
@@ -53,22 +45,59 @@ function handleOperator(operator){
     }
 };
 function handleNumber(number){
-    if(screenValue==="0"){
+    if(isNaN(screenValue)){screenValue=number}
+    else if(screenValue==="0"){
         screenValue=number;
     }else{
     screenValue+=number;
-   
     }
 };
 function buttonValue(){
+     
      document.querySelector(".calc-buttons").addEventListener("click", function(event){
-       buttonClick(event.target.innerText)});
-}
+        if(event.target.matches("button")){
+       buttonClick(event.target.innerText)
+        }
+        
+        else{
+            return
+        };
+    })};
 function rerendder(){
     screen.innerText=screenValue;
 }
 function handleMath(operator){
+     intScreenValue = parseInt(screenValue);
     
+  
+    if (screenValue==="0"){
+        return;
+    }
+    else if(runningTotal===0){
+      
+        runningTotal=intScreenValue;
+    }else{
+        flushOperation(intScreenValue);
+    }
+    
+    lastOperator=operator;
+    screenValue=operator;
+   
+
 }
+function flushOperation(intScreenValue){
+     if(lastOperator==="+"){
+        runningTotal+=intScreenValue;
+     }
+        else if(lastOperator==="-"){ 
+        runningTotal-=intScreenValue;
+     } else if(lastOperator==="×"){
+        runningTotal*=intScreenValue;
+     } else if(lastOperator==="÷"){
+        runningTotal/=intScreenValue;
+     }
+    console.log(runningTotal);};
+
 
 buttonValue();
+
